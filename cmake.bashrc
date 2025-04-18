cd0e0213c934e27e2f7e06d2727fed55178ce932
Ayu <claire@ayu.land>
2025-04-18 18:04:24 +0800

    Add CMake setup

---
+++ cmake.bashrc
@@ -0,0 +1,1 @@
+export PATH=$NOMAD/cmake-4.0.1-linux-x86_64/bin:$PATH
---
+++ cmake.sh
@@ -0,0 +1,2 @@
+curl https://github.com/Kitware/CMake/releases/download/v4.0.1/cmake-4.0.1-linux-x86_64.tar.gz -O
+tar xf cmake-4.0.1-linux-x86_64.tar.gz
