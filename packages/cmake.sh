bf2fbe91610e618fbc43290dbb15e0d4d43e3373
Ayu <claire@ayu.land>
2025-11-25 03:34:04 +0800

    A more robust Bash execute/source distinguishment method
    Ref: https://stackoverflow.com/a/28776166

--- packages/cmake.sh
+++ packages/cmake.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+if ! (return 0 2>/dev/null); then
   curl https://github.com/Kitware/CMake/releases/download/v4.0.1/cmake-4.0.1-linux-x86_64.tar.gz -O
   tar xf cmake-4.0.1-linux-x86_64.tar.gz
 else
