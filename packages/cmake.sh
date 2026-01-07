a21b84653e3fc0f2e1973905f18bdb858cd3912b
Ayu <claire@ayu.land>
2026-01-07 23:38:02 +0800

    Upgrade CMake 4.0.1 -> 4.2.1

--- packages/cmake.sh
+++ packages/cmake.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 if ! (return 0 2>/dev/null); then
-  curl https://github.com/Kitware/CMake/releases/download/v4.0.1/cmake-4.0.1-linux-x86_64.tar.gz -O
-  tar xf cmake-4.0.1-linux-x86_64.tar.gz
+  curl https://github.com/Kitware/CMake/releases/download/v4.2.1/cmake-4.2.1-linux-x86_64.tar.gz -L -O
+  tar xf cmake-4.2.1-linux-x86_64.tar.gz
 else
-  export PATH=$NOMAD/cmake-4.0.1-linux-x86_64/bin:$PATH
+  export PATH=$NOMAD/cmake-4.2.1-linux-x86_64/bin:$PATH
 fi
